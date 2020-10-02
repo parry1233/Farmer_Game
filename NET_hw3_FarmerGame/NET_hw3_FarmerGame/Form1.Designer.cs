82b09c01835836da22a606918532c1f594bdc770
Perry <49940223+parry1233@users.noreply.github.com>
2020-10-02 21:48:16 +0800

    Add files via upload

---
+++ NET_hw3_FarmerGame/NET_hw3_FarmerGame/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace NET_hw3_FarmerGame
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.right = new System.Windows.Forms.Button();
+			this.left = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 23;
+			this.listBox1.Location = new System.Drawing.Point(12, 150);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(192, 234);
+			this.listBox1.TabIndex = 0;
+			// 
+			// listBox2
+			// 
+			this.listBox2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 23;
+			this.listBox2.Location = new System.Drawing.Point(311, 150);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(192, 234);
+			this.listBox2.TabIndex = 1;
+			// 
+			// right
+			// 
+			this.right.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.right.Location = new System.Drawing.Point(210, 225);
+			this.right.Name = "right";
+			this.right.Size = new System.Drawing.Size(95, 40);
+			this.right.TabIndex = 2;
+			this.right.Text = "→";
+			this.right.UseVisualStyleBackColor = true;
+			this.right.Click += new System.EventHandler(this.Right_Click);
+			// 
+			// left
+			// 
+			this.left.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.left.Location = new System.Drawing.Point(210, 271);
+			this.left.Name = "left";
+			this.left.Size = new System.Drawing.Size(95, 40);
+			this.left.TabIndex = 3;
+			this.left.Text = "←";
+			this.left.UseVisualStyleBackColor = true;
+			this.left.Click += new System.EventHandler(this.Left_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(375, 412);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 26);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "How to play";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("新細明體", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(491, 117);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Donald the farmer";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.left);
+			this.Controls.Add(this.right);
+			this.Controls.Add(this.listBox2);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Button right;
+		private System.Windows.Forms.Button left;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ NET_hw3_FarmerGame/NET_hw3_FarmerGame/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NET_hw3_FarmerGame
+{
+	public partial class Form1 : Form
+	{
+		string farmer = "Farmer";
+		string vegetable = "Vegetable";
+		string goat = "Goat";
+		string wolf = "Wolf";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			listBox1.Items.Add(farmer);
+			listBox1.Items.Add(vegetable);
+			listBox1.Items.Add(goat);
+			listBox1.Items.Add(wolf);
+			check_condition();
+		}
+
+		private void check_condition()
+		{
+			if(listBox2.Items.Contains(farmer))
+			{
+				left.Enabled = true;
+				listBox2.Enabled = true;
+				right.Enabled = false;
+				listBox1.Enabled = false;
+				if (listBox1.Items.Contains(vegetable) &&listBox1.Items.Contains(goat))
+				{
+					dead(goat, vegetable);
+				}
+				else if (listBox1.Items.Contains(goat)&&listBox1.Items.Contains(wolf))
+				{
+					dead(wolf, goat);
+				}
+			}
+			else if (listBox1.Items.Contains(farmer))
+			{
+				right.Enabled = true;
+				listBox1.Enabled = true;
+				left.Enabled = false;
+				listBox2.Enabled = false;
+				if (listBox2.Items.Contains(vegetable) && listBox2.Items.Contains(goat))
+				{
+					dead(goat, vegetable);
+				}
+				else if (listBox2.Items.Contains(goat) && listBox2.Items.Contains(wolf))
+				{
+					dead(wolf, goat);
+				}
+			}
+
+			if(listBox2.Items.Count==4)
+			{
+				endgame();
+			}
+		}
+
+		private void Right_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (!listBox1.SelectedItem.ToString().Equals(farmer))
+				{
+					listBox2.Items.Add(listBox1.SelectedItem);
+					listBox1.Items.Remove(listBox1.SelectedItem);
+					listBox2.Items.Add(farmer);
+					listBox1.Items.Remove(farmer);
+				}
+				else if (listBox1.SelectedItem.ToString().Equals(farmer))
+				{
+					listBox2.Items.Add(farmer);
+					listBox1.Items.Remove(farmer);
+				}
+				check_condition();
+			}
+			catch(System.NullReferenceException)
+			{
+				Err();
+			}
+		}
+
+		private void Left_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (!listBox2.SelectedItem.ToString().Equals(farmer))
+				{
+					listBox1.Items.Add(listBox2.SelectedItem);
+					listBox2.Items.Remove(listBox2.SelectedItem);
+					listBox1.Items.Add(farmer);
+					listBox2.Items.Remove(farmer);
+				}
+				else if (listBox2.SelectedItem.ToString().Equals(farmer))
+				{
+					listBox1.Items.Add(farmer);
+					listBox2.Items.Remove(farmer);
+				}
+				check_condition();
+			}
+			catch(System.NullReferenceException)
+			{
+				Err();
+			}
+		}
+
+		private void dead(string one,string two)
+		{
+			MessageBox.Show("You failed!\nThe " + one + " eat the " + two + "!");
+			Application.Restart();
+		}
+		private void endgame()
+		{
+			MessageBox.Show("You Succeed!");
+			Application.Restart();
+		}
+		private void Err()
+		{
+			MessageBox.Show("Please choose a member!");
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("1.Farmer can only bring one member with him.\n2.Goat will eat the vegetable, wolf will eat the goat.\n" +
+				"3.All member need to cross the river to succeed the mission.","How to play",MessageBoxButtons.OK,MessageBoxIcon.Information);
+		}
+	}
+}
